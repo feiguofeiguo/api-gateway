bd1e2a001b8dd46f1a81a393a44a17c3c7947de3
Dangingxiong <1030227026@qq.com>
2025-07-31 09:01:33 +0800

    fix:使用线程安全集合类替换加锁操作

--- gateway-core/src/main/java/com/bank/gateway/loadbalancer/loadbalancerImpl/RoundRobin.java
+++ gateway-core/src/main/java/com/bank/gateway/loadbalancer/loadbalancerImpl/RoundRobin.java
@@ -6,23 +6,18 @@ import com.bank.gateway.router.entity.ServiceProviderInstance;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /*
  * 轮询算法做负载均衡
  */
 public class RoundRobin implements LoadBalancer {
-    private final Map<String, AtomicInteger> indexes = new HashMap<>();
+    private final Map<String, AtomicInteger> indexes = new ConcurrentHashMap<>();
 
     @Override
     public ServiceProviderInstance choose(String serviceId, List<ServiceProviderInstance> instances, String clientIP) {
-        if(!indexes.containsKey(serviceId)) {
-            synchronized (serviceId) {
-                if(!indexes.containsKey(serviceId)) {
-                    indexes.put(serviceId, new AtomicInteger(-1));
-                }
-            }
-        }
-        return instances.get(indexes.get(serviceId).incrementAndGet() % instances.size());
+        indexes.putIfAbsent(serviceId, new AtomicInteger(-1));
+        return instances.get((indexes.get(serviceId).incrementAndGet() & Integer.MAX_VALUE) % instances.size());
     }
 }
